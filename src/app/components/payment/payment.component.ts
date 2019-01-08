28f8d1e9a982df80a9c666735c70bf86a4451344
ERVIS-PC\Ervis <ervistrupja@outlook.com>
2019-01-08 20:16:23 +0100

    Stripe token implementation

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -2,108 +2,32 @@ import { Component, OnInit } from '@angular/core';
   import { of } from 'rxjs';
   import { PayPalConfig, PayPalEnvironment, PayPalIntegrationType } from 'ngx-paypal';
 
+
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.css']
 })
-export class PaymentComponent implements OnInit {
+export class PaymentComponent {
 
-    public payPalConfig?: PayPalConfig;
+  licensePrice: string = '1000';
 
-    ngOnInit(): void {
-      this.initConfig();
-    }
+  openCheckout() {
+    var handler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_oi0sKPJYLGjdvOXOM8tE8cMa',
+      locale: 'auto',
+      token: function (token: any) {
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+        console.log('Token - ',token);
+      }
+    });
 
-    private initConfig(): void {
-      this.payPalConfig = new PayPalConfig(
-        PayPalIntegrationType.ClientSideREST,
-        PayPalEnvironment.Sandbox,
-        {
-          commit: true,
-          client: {
-            sandbox:
-              'ARJuJamIHR1QeEi5KEUubjzh4Vnzq7JUN2-f6zbnatTiP1fO-fqkES8NkMMAgbdCaK7jNgEHzvvduJmF'
-          },
-          button: {
-            label: 'paypal',
-            layout: 'vertical'
-          },
-          onAuthorize: (data, actions) => {
-            console.log('Authorize');
-            return of(undefined);
-          },
-          onPaymentComplete: (data, actions) => {
-            console.log('OnPaymentComplete');
-            console.log(data);
-            console.log(actions);
-          },
-          onCancel: (data, actions) => {
-            console.log('OnCancel');
-          },
-          onError: err => {
-            console.log('OnError');
-          },
-          onClick: () => {
-            console.log('onClick');
-          },
-          validate: (actions) => {
-            console.log(actions);
-          },
-          experience: {
-            noShipping: true,
-            brandName: 'Angular PayPal'
-          },
-          transactions: [
-            {
-              amount: {
-                total: 30.11,
-                currency: 'USD',
-                details: {
-                  subtotal: 30.00,
-                  tax: 0.07,
-                  shipping: 0.03,
-                  handling_fee: 1.00,
-                  shipping_discount: -1.00,
-                  insurance: 0.01
-                }
-              },
-              custom: 'Custom value',
-              item_list: {
-                items: [
-                  {
-                    name: 'hat',
-                    description: 'Brown hat.',
-                    quantity: 5,
-                    price: 3,
-                    tax: 0.01,
-                    sku: '1',
-                    currency: 'USD'
-                  },
-                  {
-                    name: 'handbag',
-                    description: 'Black handbag.',
-                    quantity: 1,
-                    price: 15,
-                    tax: 0.02,
-                    sku: 'product34',
-                    currency: 'USD'
-                  }],
-                shipping_address: {
-                  recipient_name: 'Brian Robinson',
-                  line1: '4th Floor',
-                  line2: 'Unit #34',
-                  city: 'San Jose',
-                  country_code: 'US',
-                  postal_code: '95131',
-                  phone: '011862212345678',
-                  state: 'CA'
-                },
-              },
-            }
-          ],
-          note_to_payer: 'Contact us if you have troubles processing payment'
-        }
-      );
-    }
+    handler.open({
+      name: 'www.tekstsakte.com',
+      description: 'Shkruaj Shqip',
+      zipCode: true,
+      amount: this.licensePrice
+    });
   }
+}
