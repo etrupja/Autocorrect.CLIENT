b370affc679c098a144a747d38ad9c7707f8c09a
ERVIS-PC\Ervis <ervistrupja@outlook.com>
2019-01-09 19:37:33 +0100

    Payments set up

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
   import { PayPalConfig, PayPalEnvironment, PayPalIntegrationType } from 'ngx-paypal';
 import { PaymentService } from 'src/app/shared/services/payment.service';
 import { conditionallyCreateMapObjectLiteral } from '@angular/compiler/src/render3/view/util';
+import { ToastrManager } from 'ng6-toastr-notifications';
 
 
 @Component({
@@ -12,21 +13,46 @@ import { conditionallyCreateMapObjectLiteral } from '@angular/compiler/src/rende
 })
 export class PaymentComponent {
 
-  constructor(private service:PaymentService) { }
+  constructor(private service:PaymentService,
+              public toastr: ToastrManager) { }
 
-  licensePrice: string = '1000';
+  licensePrice: number = 2000;
+  _toastr: ToastrManager = null;
 
-  openCheckout() {
+  openCheckout(_price) {
     var handler = (<any>window).StripeCheckout.configure({
-      key: 'pk_test_oi0sKPJYLGjdvOXOM8tE8cMa',
+      key: 'pk_test_kyV742LfU6XvYlJsjx5h1lsZ',
       locale: 'auto',
+      currency:'ALL',
       token: function (token: any) {
+        token.price = _price; //set the price
         // You can access the token ID with `token.id`.
         // Get the token ID to your server-side code for use.
-        console.log('Token - ',token);
-        
         const url = 'https://localhost:44387/api/payment/charge';
         var xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+              console.log(this.responseText);
+          }
+          else if (this.readyState == 4 && this.status != 200) {
+            console.log(this.responseText);
+          }
+          else if(this.readyState == 0){
+            console.log('UNSENT');
+          }
+          else if(this.readyState == 1){
+            console.log('OPENED');
+          }
+          else if(this.readyState == 2){
+            console.log('HEADERS_RECEIVED');
+          }
+          else if(this.readyState == 3)
+          {
+            console.log('LOADING');
+          }
+      };
+
         xhr.open("POST", url);
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(JSON.stringify({
@@ -38,8 +64,7 @@ export class PaymentComponent {
     handler.open({
       name: 'www.tekstsakte.com',
       description: 'Shkruaj Shqip',
-      zipCode: true,
-      amount: this.licensePrice
+      amount: this.licensePrice*100 //500 = 5.00ALL
     });
   }
 }
